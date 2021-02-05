6917875f9abd7ed62439445bd21911c877413f62
mqian <mqian@tibco.com>
2021-02-05 10:04:56 -0800

    updated the copyright year to 2021.

--- fsrest-deployment/1.0.0/samples/docker-scripts/setenv.sh
+++ fsrest-deployment/1.0.0/samples/docker-scripts/setenv.sh
@@ -1,3 +1,8 @@
+######################################################################
+# Copyright © 2021. TIBCO Software Inc.
+# This file is subject to the license terms contained
+# in the license file that is distributed with this file.
+######################################################################
 #!/bin/bash
 
 while IFS="=" read name val
